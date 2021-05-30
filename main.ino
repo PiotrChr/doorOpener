5e722606a4a7c9dba5f63568ffd2877bac13bb70
Piotr Chrusciel <7870121+PiotrChr@users.noreply.github.com>
2021-05-30 12:41:17 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,16 +1,27 @@
 #include <CircularBuffer.h>
-#include <Servo.h>
 #include <Wire.h>
+#include <AccelStepper.h>
 
 # define I2C_SLAVE_ADDRESS 11
 
-Servo doorServo;  
+const int stepperEnPin = 2;
+const int stepPin = 3;
+const int dirPin = 4;
+int stepperState = LOW;
+int pos = 0;
+const int stepperMinSwing = 0;
+const int stepperMaxSwing = 120;
+const int stepDelayMs = 500;
+const int fullCyclePulses = 200;
 
-int pos = 0;    
-const int servoPin = 9;
+float motorMaxSpeed = 25.0;
+float motorSpeed = 25.0;   //pulse per second
+float motorAccel = 1000.0; //87500; //steps/second/second to accelerate
+AccelStepper stepper(1, stepPin, dirPin); 
+
 const int redLedPin = 8;
 const int greenLedPin = 7;
-const int buttonPin = 3;
+const int buttonPin = 6;
 const int remoteButtonPin = 5;
 
 int redLedState = HIGH;
@@ -19,17 +30,11 @@ int buttonState;
 int lastButtonState = LOW;
 
 unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 150; 
+const unsigned long debounceDelay = 150;
 unsigned long lastButtonLockTime = millis();
 const unsigned long buttonLockTime = 3000;
 
 bool locked = false;
-
-int servoState = LOW;
-const int servoStartPos = 0;
-const int servoMinSwing = 0;
-const int servoMaxSwing = 120;
-const int stepDelay = 5;
 
 const int piMessageSize = 2;
 const int piMessageOpenDoor = 100;
@@ -39,118 +44,124 @@ bool piButtonPressed = false;
 
 typedef void (*Job)();
 
-struct Task{
+struct Task {
   String name;
   unsigned long dueDate;
   Job job;
-  };
+};
 
 const int taskLimit = 20;
 CircularBuffer <Task, taskLimit> tasks;
 
 void setup() {
-//  Serial.begin(9600);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  // pinMode(servoPin, OUTPUT)
+    Serial.begin(9600);
+  setupButton();
+  setupLeds();
+  setupStepper();
   setupI2c();
-  bootServo();
 }
 
-void stopServo() {
-  doorServo.detach();
+void setupButton() {
+  pinMode(buttonPin, INPUT);
   }
 
-void startServo() {
-  if (doorServo.attached() == false) {
-      doorServo.attach(servoPin);
-    }
+void setupLeds() {
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
   }
 
-void bootServo() {
-  startServo();
+void setupStepper() {
+//   pinMode(stepPin, OUTPUT);
+   pinMode(stepperEnPin, OUTPUT);
+//   pinMode(dirPin, OUTPUT);
+    
+    stepper.setMaxSpeed(motorMaxSpeed);
+    stepper.setSpeed(motorSpeed);
+    stepper.setAcceleration(motorAccel);
+    delay(2000);
+  }
+
+void stepperOn() {
+  stepperState = HIGH;
+  }
+
+void stepperOff() {
+  stepperState = LOW;
+  }
   
-  delay(1000);
-  doorServo.write(servoStartPos);
-  delay(2000);
-
-  stopServo();
-  }
-
 void setupI2c() {
   Wire.begin(I2C_SLAVE_ADDRESS);
   delay(1000);
-//  Wire.onRequest(i2cRequest);
+  //  Wire.onRequest(i2cRequest);
   Wire.onReceive(i2cReceive);
-  }
+}
 
 void i2cReceive(int howMany) {
   if (howMany != piMessageSize) {
     return;
-    }
-  
+  }
+
   for (int i = 0; i < howMany; i++) {
     byte c = Wire.read();
     if (i == piMessageSize - 1) {
-        handleI2cMessage(c);
-      }
+      handleI2cMessage(c);
     }
   }
+}
 
 void handleI2cMessage(int message) {
-  if (message==piMessageOpenDoor) {
-      piButtonPressed = true;
-    }
-  } 
+  if (message == piMessageOpenDoor) {
+    piButtonPressed = true;
+  }
+}
 
 void i2cRequest() {
-    
-  }
+
+}
 
 void addTask(String name, unsigned long dueDate, void job ()) {
   struct Task task = {name, dueDate, job};
   tasks.push(task);
-  }
+}
 
 void performTasks() {
-  for (int i=0; i < tasks.size(); i++) {
-      if (tasks[i].dueDate < millis()) {
-        struct Task task = tasks.shift();
-        task.job();
-      } else {
-        return;
-      }   
+  for (int i = 0; i < tasks.size(); i++) {
+    if (tasks[i].dueDate < millis()) {
+      struct Task task = tasks.shift();
+      task.job();
+    } else {
+      return;
     }
-  }  
+  }
+}
 
 void toggleLed(String color) {
   if (color == "red") {
-      redLedState = HIGH;
-      greenLedState = LOW;
-    }
+    redLedState = HIGH;
+    greenLedState = LOW;
+  }
 
   if (color == "green") {
-      redLedState = LOW;
-      greenLedState = HIGH;
-    }  
+    redLedState = LOW;
+    greenLedState = HIGH;
   }
+}
 
 void checkButton() {
   int reading = piButtonPressed;
   if (reading == false) {
     reading = digitalRead(buttonPin);
-    }
-  
+  }
+
   unsigned long now = millis();
 
   if ((now - lastButtonLockTime) < buttonLockTime) {
-      return;
-    }
+    return;
+  }
 
-//  if (reading != lastButtonState) {
-//    lastDebounceTime = now;
-//    }
+  //  if (reading != lastButtonState) {
+  //    lastDebounceTime = now;
+  //    }
 
   if ((now - lastDebounceTime) > debounceDelay) {
     if (reading != buttonState) {
@@ -158,42 +169,62 @@ void checkButton() {
       lastButtonLockTime = now;
       if (buttonState == HIGH) {
         buttonPressed();
-        }
-      
       }
+
     }
   }
-
-void toggleServo(int state) {
-  if (state == servoState) {
-    return;
-    }
-
-  if (state == HIGH) {
-    for (int pos = servoStartPos; pos <= servoMaxSwing + servoStartPos; pos += 1) {
-      doorServo.write(pos);
-      delay(stepDelay);
-      }
-  } else {
-    for (int pos = servoMaxSwing+servoStartPos; pos >= servoStartPos; pos -= 1) {
-      doorServo.write(pos);
-      delay(stepDelay);
-      } 
-    }
-    
-    servoState = state;
 }
+
+void stepperDir(String dir = "right") {
+  if (dir == "right") {
+    digitalWrite(dirPin,HIGH);
+    } else {
+      digitalWrite(dirPin,LOW);
+      }
+  }
+
+void moveStepper(float rotation, String dir, int relSpeed = 1) {
+//  stepperDir(dir);
+
+  int pulses = (int)(rotation * fullCyclePulses) * ((dir == "right") ? 1 : -1);
+  int delayMs = (int)(stepDelayMs / relSpeed);
+
+  Serial.println(pulses);
+  stepper.runToNewPosition(pulses);
+//  for(int x = 0; x < pulses; x++) {
+//    digitalWrite(stepPin,HIGH); 
+//    delayMicroseconds(stepDelayMs); 
+//    digitalWrite(stepPin,LOW); 
+//    delayMicroseconds(delayMs); 
+//    }
+  }
 
 void buttonPressed() {
   piButtonPressed = false;
-  
+
   unsigned long now = millis();
-  addTask("StartServo", (now), []() {startServo();});
-  addTask("ServoHigh", (now+1000), []() {toggleServo(HIGH);});
-  addTask("GreenOn", (now+2000), []() {toggleLed("green");});
-  addTask("RedOn", (now+5000), []() {toggleLed("red");});
-  addTask("ServoLow", (now+5500), []() {toggleServo(LOW);});
-  addTask("StopServo", (now+7000), []() {stopServo();});
+  addTask("StartStepper", (now), []() {
+    stepperOn();
+  });
+  addTask("MoveRight", (now + 1000), []() {
+    moveStepper(0.3, "right", 1);
+  });
+  addTask("GreenOn", (now + 2000), []() {
+    toggleLed("green");
+  });
+  addTask("RedOn", (now + 5000), []() {
+    toggleLed("red");
+  });
+//  addTask("MoveLeft", (now + 5500), []() {
+//    moveStepper(0.3, "left", 1);
+//  });
+  addTask("StopStepper", (now + 6500), []() {
+    stepperOff();
+  });
+}
+
+void checkStepper() {
+  digitalWrite(stepperEnPin, stepperState == LOW ? HIGH : LOW);
   }
 
 void checkLeds() {
@@ -204,6 +235,7 @@ void checkLeds() {
 void loop() {
   checkButton();
   checkLeds();
+  checkStepper();
 
   performTasks();
 }
